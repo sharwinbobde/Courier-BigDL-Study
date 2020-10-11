d6d417e831123e569cb9877e2ea8b359578a63c7
diegostock12 <diego.albo.martinez@gmail.com>
2020-10-11 19:50:59 +0200

    Created simplenet for MNIST

---
+++ simplenet.py
@@ -0,0 +1,61 @@
+from optparse import OptionParser
+from bigdl.dataset.transformer import *
+from bigdl.models.lenet.utils import *
+from bigdl.nn.layer import *
+from bigdl.nn.criterion import *
+from bigdl.optim.optimizer import *
+from bigdl.util.common import *
+
+"""Simple fully connected model for MNIST"""
+
+def build_model(class_num):
+    model = Sequential()
+    model.add(Reshape([1,28,28]))
+    model.add(Linear(28 * 28, 128))
+    model.add(ReLU())
+    model.add(Linear(128, class_num))
+    model.add(LogSoftMax())
+
+    return model
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-a", "--action", dest="action", default="train")
+    parser.add_option("-b", "--batchSize", type=int, dest="batchSize", default="128")
+    parser.add_option("-o", "--modelPath", dest="modelPath", default="/tmp/simplenet/model.470")
+    parser.add_option("-c", "--checkpointPath", dest="checkpointPath", default="/tmp/simplenet")
+    parser.add_option("-t", "--endTriggerType", dest="endTriggerType", default="epoch")
+    parser.add_option("-n", "--endTriggerNum", type=int, dest="endTriggerNum", default="20")
+    parser.add_option("-d", "--dataPath", dest="dataPath", default="/tmp/mnist")
+
+    (options, args) = parser.parse_args(sys.argv)
+
+    sc = SparkContext(appName="simplenet", conf=create_spark_conf())
+    redire_spark_logs()
+    show_bigdl_info_logs()
+    init_engine()
+
+    if options.action == "train":
+        (train_data, test_data) = preprocess_mnist(sc, options)
+
+        optimizer = Optimizer(
+            model=build_model(10),
+            training_rdd=train_data,
+            criterion=ClassNLLCriterion(),
+            optim_method=SGD(learningrate=0.01, learningrate_decay=0.0002),
+            end_trigger=get_end_trigger(options),
+            batch_size=options.batchSize)
+        validate_optimizer(optimizer, test_data, options)
+        trained_model = optimizer.optimize()
+        parameters = trained_model.parameters()
+    elif options.action == "test":
+        # Load a pre-trained model and then validate it through top1 accuracy.
+        test_data = get_mnist(sc, "test", options.dataPath) \
+            .map(lambda rec_tuple: (normalizer(rec_tuple[0], mnist.TEST_MEAN, mnist.TEST_STD),
+                                    rec_tuple[1])) \
+            .map(lambda t: Sample.from_ndarray(t[0], t[1]))
+        model = Model.load(options.modelPath)
+        results = model.evaluate(test_data, options.batchSize, [Top1Accuracy()])
+        for result in results:
+            print(result)
+    sc.stop()
