d23b087d4a54c19cfeed69a9039b30c1edde2405
diegostock12 <diego.albo.martinez@gmail.com>
2020-10-16 22:06:09 +0200

    Added the notebook for reading the experiments from the folders

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 config = {
-    "SSH_USER": "",
-    "BIGDL_USER": "",
-    "GCLOUD_ACCOUNT": "",
-    "GCLOUD_KEY_PATH": "",
-    "GCLOUD_PROJECT": "",
-    "PKEY": "",
+    "SSH_USER": "diego",
+    "BIGDL_USER": "am72ghiassi",
+    "GCLOUD_ACCOUNT": "libcloud@qpe-big-dl.iam.gserviceaccount.com",
+    "GCLOUD_KEY_PATH": "./qpe.json",
+    "GCLOUD_PROJECT": "qpe-big-dl",
+    "PKEY": "./google_compute_engine",
     "DESIGN_CSV": "",
 }
