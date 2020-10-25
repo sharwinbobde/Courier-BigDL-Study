9bafdaee9ecb1d1f967251833b4b80b205927da5
Diego Albo Martínez <diego.albo.martinez@gmail.com>
2020-10-25 19:25:09 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 config = {
-    "SSH_USER": "diego",
-    "BIGDL_USER": "am72ghiassi",
-    "GCLOUD_ACCOUNT": "libcloud@qpe-big-dl.iam.gserviceaccount.com",
-    "GCLOUD_KEY_PATH": "./qpe.json",
-    "GCLOUD_PROJECT": "qpe-big-dl",
-    "PKEY": "./google_compute_engine",
+    "SSH_USER": "",
+    "BIGDL_USER": "",
+    "GCLOUD_ACCOUNT": "",
+    "GCLOUD_KEY_PATH": "",
+    "GCLOUD_PROJECT": "",
+    "PKEY": "",
     "DESIGN_CSV": "",
 }
